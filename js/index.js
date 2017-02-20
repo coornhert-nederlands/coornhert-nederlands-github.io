6d49276473893690c8b15c4e4e4a82725167bacb
coornhert-nederlands <3pietermantel@gmail.com>
2017-02-20 11:08:35 +0100

    Nav design #2
    made the nav bar functional

--- js/index.js
+++ js/index.js
@@ -1,3 +1,14 @@
 $(document).ready(function() {
-	
+	$("nav").children().find(".tab-main").children().find("ul").slideUp(0);
+	$("nav").children().find(".tab-main").click(function() {
+		var button = this;
+		if($(button).hasClass("active")) {
+			$(button).removeClass("active");
+		} else {
+			$("nav").children().find(".tab-main").removeClass("active");
+			$(button).addClass("active");
+			$("nav").children().find(".tab-main").children().find("ul").slideUp();
+		}
+		$(button).children().find("ul").slideToggle();
+	});
 });
