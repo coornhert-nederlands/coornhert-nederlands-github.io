f7855142b3a7b7e2d1ae5484ed1633e2a8e9e350
coornhert-nederlands <3pietermantel@gmail.com>
2017-02-23 13:42:16 +0100

    Reader #1

--- js/index.js
+++ js/index.js
@@ -1,4 +1,5 @@
 $(document).ready(function() {
+	//==============================================Tabs============================================
 	$("nav").children().find(".tab-main").children().find("ul").slideUp(0);
 	$("nav").children().find(".tab-main").click(function() {
 		var button = this;
@@ -11,4 +12,14 @@ $(document).ready(function() {
 		}
 		$(button).children().find("ul").slideToggle();
 	});
+	
+	//============================================READ==============================================
+	// Check for the various File API support.
+	if (window.File && window.FileReader && window.FileList && window.Blob) {
+		// Great success! All the File APIs are supported.
+	} else {
+		alert('The File APIs are not fully supported in this browser.');
+	}
+	
+	
 });
